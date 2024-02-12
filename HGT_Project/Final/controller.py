c3cc87f8e96899175899189a2ce1c07e283f8ec0
Cole Malinchock <113946722+ColeMalinchock1@users.noreply.github.com>
2024-02-12 12:39:58 -0500

    Update controller.py

--- HGT_Project/Final/controller.py
+++ HGT_Project/Final/controller.py
@@ -3,8 +3,11 @@
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
+from json_server import JSON_Server
 import time
 import math
+from serial import Serial
+import pyTMCL
 
 
 class Controller:
@@ -25,6 +28,18 @@ class Controller:
         if self.hxMode:
             from hx711 import HX711
             
+        # Setting up TMCL serial communication
+        # Getting the motor address, default is 1
+        MODULE_ADDRESS = 1
+
+        # Getting the serial port
+        serial_port = Serial("/dev/ttyACM0")
+
+        # Connecting to the serial port
+        bus = pyTMCL.connect(serial_port)
+
+        # Creating the motor object that can be controlled
+        self.motor = bus.get_motor(MODULE_ADDRESS)
 
         # GPIO Pins
         self.increase_tension = 22 # Stop_R
@@ -657,228 +672,60 @@ class Controller:
 
     # Controls is able to compare the current tension to the set point to see if it's within the dead band and within the slow mode range
     def controls(self):
+        
         import RPi.GPIO as GPIO
+
+        # Getting the most current tension
         self.current_tension = self.get_tension()
         # If the time is greater than or equal to the set pause period, it will continue the controls
-        if time.time() - self.last_time_delay > self.setTimeDelay and time.time() - self.set_point_time > 2:
+        if time.time() - self.last_time_delay > self.setTimeDelay:
+            
+            # Finding the difference between the set point and current tension
+            diff = self.setWeight - self.current_tension
 
-            # If the current tension is below the dead band the tension is increased
-            if (self.setWeight - self.dead_band_range) > self.current_tension:
+            # Coefficient of speed
+            k = 1
 
-                # If the current tension is below the slow mode range the speed is fast
-                if self.setWeight - self.slow_mode_range > self.current_tension:
-                    GPIO.output(self.slow_mode , GPIO.LOW)
-                    GPIO.output(self.yellow_led , GPIO.LOW)
-                    speed = "Fast"
-                else:
-                    GPIO.output(self.slow_mode , GPIO.HIGH)
-                    GPIO.output(self.yellow_led , GPIO.HIGH)
-                    speed = "Slow"
-                GPIO.output(self.increase_tension , GPIO.HIGH)
-                GPIO.output(self.green_led , GPIO.HIGH)
-                
-                GPIO.output(self.decrease_tension , GPIO.LOW)
-                GPIO.output(self.red_led , GPIO.LOW)
+            # Setting the speed of the motor based on the distance to the setpoint
+            velocity = diff * k
+
+            # If the difference is negative and outside the dead band range, the tension is decreased
+            if (diff + self.dead_band_range) < 0:
+                self.motor.rotate_left(velocity)
+                GPIO.output(self.red_led, GPIO.HIGH)
+                GPIO.output(self.green_led, GPIO.LOW)
+                speed = str(velocity)
+                turning_direction = "Decreasing Tension"
+
+            # If the difference is positive and outside the dead band range, the tension is increased
+            if (diff - self.dead_band_range) > 0:
+                self.motor.rotate_right(velocity)
+                GPIO.output(self.green_led, GPIO.HIGH)
+                GPIO.output(self.red_led, GPIO.LOW)
+                speed = str(velocity)
                 turning_direction = "Increasing Tension"
             
-            # If the current tension is above the dead band the tension is decreased
-            elif self.setWeight + self.dead_band_range < self.current_tension:
-
-                # If the current tension is above the slow mode range the speed is fast
-                if self.setWeight + self.slow_mode_range < self.current_tension:
-                    GPIO.output(self.slow_mode , GPIO.LOW)
-                    GPIO.output(self.yellow_led , GPIO.LOW)
-                    speed = "Fast"
-                else:
-                    GPIO.output(self.slow_mode , GPIO.HIGH)
-                    GPIO.output(self.yellow_led , GPIO.HIGH)
-                    speed = "Slow"
-                GPIO.output(self.decrease_tension , GPIO.HIGH)
-                GPIO.output(self.red_led , GPIO.HIGH)
-                
-                GPIO.output(self.increase_tension , GPIO.LOW)
-                GPIO.output(self.green_led , GPIO.LOW)
-
-                turning_direction = "Decreasing Tension"
-            
             else:
+                self.motor.stop()
                 speed = ""
                 turning_direction = "Stopped"
-                self.set_point_time = time.time()
-                for i in self.output_pins:
-                    GPIO.output(i , GPIO.LOW)
+                GPIO.output(self.green_led, GPIO.LOW)
+                GPIO.output(self.red_led, GPIO.LOW)
+                
+                
         
         # If it is less than the delay, no adjustments are to be made
         else:
-             turning_direction = "Stopped"
-             speed = ""
-             for i in self.output_pins:
-                GPIO.output(i+1 , GPIO.LOW)
+            self.motor.stop()
+            turning_direction = "Stopped"
+            speed = ""
+            GPIO.output(self.green_led, GPIO.LOW)
+            GPIO.output(self.red_led, GPIO.LOW)
+            
         
         print(speed)
         print(turning_direction)
-        return turning_direction , speed
     
-    def JSON_Server(self):
-        import requests
-        import json
-        import base64
-
-        # Opening the local file and getting the contents
-        with open(self.file_path , 'r') as file1 , open(self.file_backup_path , 'r') as file2:
-                updated_file_content = json.loads(file1.read())
-                updated_file_backup_content = json.loads(file2.read())
-
-        # Getting time
-        timer = time.time()
-
-        # Getting date
-        date = str(time.asctime())
-
-        # Formatted so that date is Month , Day , Time (hr:mi:se)
-        date = date.split()
-        date = date[1:4]
-
-        # Getting tension
-        tension = self.current_tension
-
-        # Tension setpoint
-        tension_setpoint = 12
-
-        # Creating new data on tension and time
-        patient1_data = {
-        "date": date,
-        "time": timer,
-        "tension": str(tension),
-        "tension_setpoint": str(tension_setpoint)
-        }
-
-        patient2_data = {
-        "date": date,
-        "time": timer,
-        "tension": str((tension + 1) * 2),
-        "tension_setpoint": str(tension_setpoint)
-        }
-        
-
-        # Range of short graph
-        # 1 hour range (Every 0.1 second)
-        max_size = 36000
-        # try:
-        if len(updated_file_content["data"][0][self.patient1_name][1]["patient data"]) > max_size:
-                updated_file_content["data"][0][self.patient1_name][1]["patient data"].pop(0)
-                updated_file_content["data"][1][self.patient2_name][1]["patient data"].pop(0)
-                print("Removing old data")
-        else:
-                pass
-        # except:
-        #         print("Making new patient")
-
-        # Try to add data to current patient, if patient not found, it creates a new patient in the dict and adds the data there.
-        #try:
-
-        # Appending new data to current patient
-        print("Appending new data to current patient")
-        updated_file_content["data"][0][self.patient1_name][1]["patient data"].append(patient1_data)
-        updated_file_content["data"][1][self.patient2_name][1]["patient data"].append(patient2_data)
-
-        # Appending new data to backup content and setting how often it updates
-        minutes = 10
-
-        if time.time() - self.last_time_JSON > 60:
-                keep_waiting = False
-
-        if int(str(time.asctime()).split()[3][3:5]) % minutes == 0 and not keep_waiting:
-                print("Appending new data to backup")
-                updated_file_backup_content["data"][0][self.patient1_name][1]["patient data"].append(patient1_data)
-                updated_file_backup_content["data"][1][self.patient2_name][1]["patient data"].append(patient2_data)
-                keep_waiting = True
-                self.last_time_JSON = time.time()
-
-
-        # except:
-        #         # Creating dict for patient information
-        #         patient1_info = {"info": [self.patient1_info]}
-        #         patient2_info = {"info": [self.patient2_info]}
-
-        #         patient1_data = {"patient data": [patient1_data]}
-        #         patient2_data = {"patient data": [patient2_data]}
-
-        #         i = len(updated_file_content["data"])
-
-        #         # Appending new patient dict and the new data
-        #         updated_file_content["data"].append({self.patient1_name: [patient1_info]})
-        #         updated_file_content["data"][i][self.patient1_name].append(patient1_data)
-
-        #         updated_file_content["data"].append({self.patient2_name: [patient2_info]})
-        #         updated_file_content["data"][i + 1][self.patient2_name].append(patient2_data)
-
-
-        #         updated_file_backup_content["data"].append({self.patient1_name: [patient1_info]})
-        #         updated_file_backup_content["data"][i][self.patient1_name].append(patient1_data)
-
-        #         updated_file_backup_content["data"].append({self.patient2_name: [patient2_info]})
-        #         updated_file_backup_content["data"][i + 1][self.patient2_name].append(patient2_data)
-
-        #         print("Patients created")
-
-        # Writing the updated file to the local computer
-        with open(self.file_path , 'w') as file1 , open(self.file_backup_path , 'w') as file2:
-                json.dump(updated_file_content , file1 , indent = 4)
-                json.dump(updated_file_backup_content , file2 , indent = 4)
-                print("Data added to local file")
-
-        # if time.time() - self.last_time_get_JSON > self.wait_time:
-        #     # Making a GET request for current file from GitHub
-        #     response = requests.get(self.api_url, headers=self.headers)
-        #     response_backup = requests.get(self.api_backup_url , headers = self.headers)
-        #     self.last_time_get_JSON = time.time()
-
-        
-        #     # If GET request is approved
-        #     if (response.status_code == 200 and response_backup.status_code == 200):
-
-        #             # Receiving current data
-        #             current_data = response.json()
-        #             current_sha = current_data['sha']
-
-        #             current_backup_data = response_backup.json()
-        #             current_backup_sha = current_backup_data['sha']
-
-        #             # Create the updated file using local data
-        #             updated_file = {
-        #             'message': 'Update JSON file',
-        #             'content': base64.b64encode(json.dumps(updated_file_content).encode()).decode(),
-        #             'sha': current_sha  # Include the current sha
-        #             }
-
-        #             updated_backup_file = {
-        #             'message': 'Update backup JSON file',
-        #             'content': base64.b64encode(json.dumps(updated_file_backup_content).encode()).decode(),
-        #             'sha': current_backup_sha  # Include the current sha
-        #             }
-
-        #             # Send a PUT request to update the file
-        #             response = requests.put(self.api_url, headers = self.headers , json = updated_file)
-        #             response_backup = requests.put(self.api_backup_url , headers = self.headers , json = updated_backup_file)
-        #             self.last_time_put_JSON = time.time()
-
-        #             # If statement for put request
-        #             if response.status_code == 200:
-        #                     print('File successfully updated on GitHub')
-        #             else:
-        #                     print('Failed to update the file. Status code: ' , response.status_code)
-        #                     print('Response: ' , response.json)
-
-        #             # If statement for put request
-        #             if response_backup.status_code == 200:
-        #                     print('File successfully updated on GitHub')
-        #             else:
-        #                     print('Failed to update the file. Status code: ' , response_backup.status_code)
-        #                     print('Response: ' , response_backup.json)
-
-            #else:
-             #       print('Failed to retrieve the current file data. Status code:', response.status_code)
-              #      print('Response:', response.json())
-        #else:
-        #        print("Waiting to get and put on Github")
+    def save_data(self):
+        JSON_Server.save_json(self.current_tension, self.setWeight)
+        JSON_Server.push_to_git()
